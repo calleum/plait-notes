23d5ef67b350943ecf8a98ecad488f750bebe89d
Calleum Pecqueux <hi@calleum.au>
2023-02-19 13:51:37 +1100

    define Value, Conditional, add test macro and tests

--- learning.rkt
+++ learning.rkt
@@ -1,29 +1,43 @@
 #lang plait
 ; We are defining a new type, `Exp`
 (define-type  Exp
-  ; There are two ways of making an `Exp`:
-  ; through the constructor `num`:
-  ; -- A `num` takes on argument
+  ; There are three ways of making an `Exp`:
+  ; Through the constructor `num`:
+  ; -- A `num` takes one argument
   ; -- That argument must be an actual number
-  [num (n : Number)] 
-  ; The other way is through the constructor `plus`:
+  [numE (n : Number)]
+  ; Through the constructor `bool`:
+  ; -- A `book` takes one arguments
+  ; -- That argument must be a boolean value
+  [boolE (b : Boolean)]
+  ; Through the constructor `plus`:
   ; -- A `plus` takes two arguments
   ; -- Both arguments must be `Exp`s
-  [plus (left : Exp) (right : Exp)])
+  [plusE (left : Exp) (right : Exp)]
+  ; Through the constructor `cnd`:
+  ; -- A `cnd` takes three arguments
+  ; -- All three are `Exp`s
+  [cndE (test : Exp) (then : Exp) (else : Exp)])
 ; this allows us to create an AST, where the program:
 ;     1 + 2
 ; will be represented as:
-;     (plus (num 1) (num 2))
+;     (plusE (numE 1) (numE 2))
 ; In short, ASTs are tree structured data that *represent programs in programs*.
-;
+
+; Here, we introduce the `Value` datatype to represent the types of
+; answers our evaluator can produce:
+(define-type Value
+  [numV (the-number : Number)]
+  [boolV (the-boolean : Boolean)])
 
 ;; Next, we need to write a parser:
 ; First, we need a conditional to check what kind of s-exp we were given:
 (define (parse s)
   (cond
     [(s-exp-number? s)
-     ; If it's a numeric s-exp, then we need to extract the number and pass it to the `num` constructor:
-     (num (s-exp->number s))]
+     ; If it's a numeric s-exp, then we need to extract the number and pass it
+     ; to the `num` constructor:
+     (numE (s-exp->number s))]
     ; Otherwise, we need to extract the list and check whether the first thing in the list is an addition
     ; symbol. 
     [(s-exp-list? s)
@@ -31,7 +45,7 @@
        (if (symbol=? '+
                      (s-exp->symbol (first l)))
            ; This models the expected expression with a recurrence
-           (plus (parse (second l))
+           (plusE (parse (second l))
                  (parse (third l)))
            ; If the first thing in the list is not an addition symbol
            ; then we signal an error:
@@ -40,16 +54,16 @@
 ;
 ; This will stop the output of all tests being printed...
 (print-only-errors #true)
-(test (parse `1) (num 1))
-(test (parse `2.3) (num 2.3))
-(test (parse `{+ 1 2}) (plus (num 1) (num 2)))
+(test (parse `1) (numE 1))
+(test (parse `2.3) (numE 2.3))
+(test (parse `{+ 1 2}) (plusE (numE 1) (numE 2)))
 (test (parse `{+ 1
                  {+ {+ 2 3}
                     4}})
-      (plus (num 1)
-            (plus (plus (num 2)
-                        (num 3))
-                  (num 4))))
+      (plusE (numE 1)
+            (plusE (plusE (numE 2)
+                        (numE 3))
+                  (numE 4))))
 ; To write negative test cases, we can use `test/exn`:
 (test/exn (parse `{1 + 2}) "")
 ; The second parameter of the test above is the expected error message of the test result ->
@@ -65,47 +79,84 @@
 ;; Because we are using `parse` to produce output to be used
 ; as input for some function `calc`, we can compose the two into a test helper function.
 
+;; Defining an add operator:
+(define (add v1 v2)
+  (type-case Value v1
+    [(numV n1)
+     (type-case Value v2
+       [(numV n2) (numV (+ n1 n2))]
+       [else (error '+ "expects RHS to be a number")])]
+     [else (error '+ "expects LHS to be a number")]))
+
+;; Defining a boolean-decision operator:
+;
+(define (boolean-decision v)
+  (type-case Value v
+    [(boolV b) b]
+    [else (error 'if "expects conditional to evaluate to a boolean")]))
+
+
 ;; Defining an evaluator:
 ;
-(calc : (Exp -> Number))
+(calc : (Exp -> Value))
 
 (define (calc e)
   ; Given some `Exp` we will want to take it apart using `type-case`,
-  ; which tells us there are two options, each with additional metadata:
+  ; which tells us there are multiple options, each with additional metadata:
   (type-case Exp e
-    ; if the whole expressions is already a number, just return it:
-    [(num n) n]
-    ; else we have to add the two sides, first converting them
-    ; from an expression into a number.
-    [(plus l r) (+ (calc l) (calc r))]))
+    ; if the whole expression is already a number, return the value type
+    ; for a number from the expression type.
+    [(numE n) (numV n)]
+    ; if the whole expression is a boolean, return the value type
+    ; for a boolean from the expression type.
+    [(boolE b) (boolV b)]
+    ; if there are two expressions, we have to add the two sides,
+    ; first converting them from an expression into a number.
+    [(plusE l r) (add (calc l) (calc r))]
+    ; if there are three expressions, we first need to recursively evaulate
+    [(cndE c t e) (if (boolean-decision (calc c))
+                     (calc t)
+                     (calc e))]))
 
 ;; Let's write some tests for the calc evaluator
+(test/exn (calc (plusE (numE 4) (boolE #false))) "RHS")
+(test (calc (numE 1)) (numV 1))
+(test (calc (numE 2.3)) (numV 2.3))
+(test (calc (plusE (numE 1) (numE 2))) (numV 3))
+(test (calc (plusE (plusE (numE 1) (numE 2))
+                  (numE 3)))
+      (numV 6))
+(test (calc (plusE (numE 1)
+                  (plusE (numE 2) (numE 3))))
+      (numV 6))
+(test (calc (plusE (numE 1)
+                  (plusE (plusE (numE 2)
+                              (numE 3))
+                        (numE 4))))
+      (numV 10))
 
-(test (calc (num 1)) 1)
-(test (calc (num 2.3)) 2.3)
-(test (calc (plus (num 1) (num 2))) 3)
-(test (calc (plus (plus (num 1) (num 2))
-                  (num 3)))
-      6)
-(test (calc (plus (num 1)
-                  (plus (num 2) (num 3))))
-      6)
-(test (calc (plus (num 1)
-                  (plus (plus (num 2)
-                              (num 3))
-                        (num 4))))
-      10)
+; Test with decimals
+; (test (calc (plusE (numE 0.1) (numE 0.2))) 0.3)
 
 ;; Here, combine the `parse` and `calc` functions to
-; `run` an  integration test:
-(run : (S-Exp -> Number))
-
+; `run` an  integration test with the plait syntax directly
+;; e.g. use `{+ 1 2} instead of (calc (plusE (numE 1) (numE 2)))
+(run : (S-Exp -> Value))
 (define (run s)
   (calc (parse s)))
 
-(test (run `1) 1)
-(test (run `2.3) 2.3)
-(test (run `{+ 1 2}) 3)
-(test (run `{+ {+ 1 2} 3}) 6)
-(test (run `{+ 1 {+ 2 3}}) 6)
-(test (run `{+ 1 {+ {+ 2 3} 4}}) 10)
+;; Extending from the combine function above, we can create a macro to allow
+; passing in a `Number` directly for the expected output.
+(define (testNum t e)
+  (test t (numV e)))
+; Rather than manually type the AST node:
+(test (run `1) (numV 1))
+; provide the expected number: 
+(testNum (run `1) 1)
+
+(testNum (run `1) 1)
+(testNum (run `2.3) 2.3)
+(testNum (run `{+ 1 2}) 3)
+(testNum (run `{+ {+ 1 2} 3}) 6)
+(testNum (run `{+ 1 {+ 2 3}}) 6)
+(testNum (run `{+ 1 {+ {+ 2 3} 4}}) 10)
