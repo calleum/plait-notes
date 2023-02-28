47aa705aa23e43a2b7f0aa8a8a2c410bf8c1867f
Calleum Pecqueux <hi@calleum.au>
2023-02-28 22:27:37 +1100

    objects, constructors, msg passing

--- obj.rkt
+++ obj.rkt
@@ -25,11 +25,124 @@
 ;; From this, a `lambda` is an object which only has _one_ entrypoint
 ;; and as such, we don't need to disambiguate which entry point to call.
 
-;; Although, it's not a good experience to use the above syntax to call 
-;; a 'method' within the object, so we can define a function to apply the 
+;; Although, it's not a good experience to use the above syntax to call
+;; a 'method' within the object, so we can define a function to apply the
 ;; call against the object, removing one paren:
 (define (msg o m . a)
   (apply (o m) a))
 ;; Notice the '.' - this is Racket's `variable-arity`?? syntax
 ;; `. a` means bind the remaining 0-* arguments to a list named `a`.
 (test (msg o 'add1 5) 6)
+
+;; Because the object is defined by the language as syntax sugaring,
+;; there is no mapping from the target back to the implementation in
+;; the orginal form, before desugaring.
+;; Because of this, some static analysis tools must implement a
+;; functionality such as this to map the expanded form being analysed
+;; back to the source code.
+
+;; CONSTRUCTORS
+;; In this language, a constructor is a function that is bound to an
+;; object - and invoked at that object's construction time.
+(define (o-constr x)
+  (lambda (m)
+    (case m
+      [(addX) (lambda (y) (+ x y))])))
+;; using the constructor as such:
+(test (msg (o-constr 5) 'addX 3) 8)
+(test (msg (o-constr 2) 'addX 3) 5)
+
+;; CLASSES
+;; In order to encapsulate state, we can use variables in the desugared
+;; language to mutate the shared state of an object
+(define (mk-o-state count)
+  (lambda (m)
+    (case m
+      [(inc) (lambda () (set! count (+ count 1)))]
+      [(dec) (lambda () (set! count (- count 1)))]
+      [(get) (lambda () count)])))
+;; using the shared state/mutation
+(test (let ([o (mk-o-state 5)])
+        (begin
+          (msg o 'inc)
+          (msg o 'inc)
+          (msg o 'dec)
+          (msg o 'get)))
+      6)
+;; when we have multiple objects, mutating one doesn't affect another:
+(test (let ([o1 (mk-o-state 3)] [o2 (mk-o-state 3)])
+        (begin
+          (msg o1 'inc)
+          (msg o1 'inc)
+          (+ (msg o1 'get) (msg o2 'get))))
+      (+ 5 3))
+
+;; PRIVATE MEMBERS
+;; data hiding is a common feature in object oriented languages,
+;; and should be straight forward to implement here using lexical scoping
+;; to ensure that attempts to access count from outside of the object
+;; would fail.
+(define (mk-o-state/priv init)
+  (let ([count init])
+    (lambda (m)
+      (case m
+        [(inc) (lambda () (set! count (+ count 1)))]
+        [(dec) (lambda () (set! count (- count 1)))]
+        [(get) (lambda () count)]))))
+
+;; STATIC MEMBERS
+;; Static memebers are shared between all instances of an object:
+(define mk-o-static
+  (let ([counter 0])
+    (lambda (amount)
+      (begin
+        (set! counter (+ 1 counter))
+        (lambda (m)
+          [(inc) (lambda (n) (set! amount (+ amount n)))]
+          [(dec) (lambda (n) (set! amount (- amount n)))]
+          [(get) (lambda () amount)]
+          [(count) (lambda() counter)])))))
+;; use the static initialisation and ensure that the global count is
+;; incremented
+(test (let ([o (mk-o-static 1000)])
+        (msg o 'count))
+      1)
+(test (let ([o (mk-o-static 0)])
+        (msg o 'count))
+      2)
+
+;; note: this construct is tied to the object... we should be able to
+;; access a static member, since this is owned by the _class_ and not
+;; by the object itself.
+
+;; Self reference
+;; it is expected that members within an object can directly reference
+;; one another, and a method has a way of reaching into it's
+;; enclosing object and accessing the methods and state within it.
+;; This is usually a reference to itself called `this` or `self`.
+
+;; Self reference using mutation
+(define o-self!
+  (let ([self 'dummy])
+    (begin
+      (set! self
+            (lambda m
+              (case m
+                [(first) (lambda (x) (msg self 'second (+ x 1)))]
+                [(second) (lambda (x) (+ x 1))])))
+      self)))
+;; testing the usage of self refrence:
+(test (msg o-self! 'first 5) 7)
+
+;; Alternatively, self refrence can be implemented without mutation,
+;; where we send the object itself as a parameter of the function/
+;; message passing
+(define o-self-no!
+  (lambda (m)
+    (case m
+      [(first) (lambda (self x) (msg/self self 'second (+ x 1)))]
+      [(second) (lambda (self x) (+ x 2))])))
+;; we now have to modify the method invocation on the object to allow
+;; passing in an object reference as a parameter
+(define (msg/self o m . a)
+  (apply (o m) o a))
